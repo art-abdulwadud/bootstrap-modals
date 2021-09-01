fd4c9594e7ebad377e243bbab1b409cfc9239af0
art-abdulwadud <abdulwadudhajike@gmail.com>
2021-09-01 17:10:54 +0300

    complete form which adds new cards to list of cards

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const myForm = document.querySelector('.my-form');
+const newImageElement = document.querySelector('#new-image');
+const newTitle = document.querySelector('#new-title');
+const newDes = document.querySelector('#new-des');
+
+let newImageValue;
+
+newImageElement.addEventListener('change', (ev) => {
+    const [file] = newImageElement.files;
+    if (file) newImageValue = URL.createObjectURL(file);
+})
+
+const addNewElement = (element, classes, value = null) => {
+    const newElement = document.createElement(element);
+    classes.forEach(key => newElement.classList.add(key));
+    newElement.innerHTML = value;
+    return newElement;
+}
+
+const addNewCard = (image, title, des) => {
+    const card = addNewElement('div', ['card']);
+    const cardImage = addNewElement('img', ['card-img-top']);
+    cardImage.src = image;
+    const cardBody = addNewElement('div', ['card-body']);
+    const cardTitle = addNewElement('h5', ['card-title'], title);
+    const cardDes = addNewElement('p', ['card-tex'], des);
+    card.append(cardImage);
+    card.append(cardBody);
+    cardBody.append(cardTitle);
+    cardBody.append(cardDes);
+    return card;
+}
 
 myForm.addEventListener('submit', (ev) => {
     ev.preventDefault();
-    alert('Form submited!!')
+    const newCard = addNewCard(newImageValue, newTitle.value, newDes.value);
+    document.querySelector('.cards').append(newCard);
 })
